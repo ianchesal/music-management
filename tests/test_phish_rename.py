4df4be5885d763b903946d881ad23af0f0d9b25a
Ian Chesal <ian.chesal@gmail.com>
2026-05-01 09:51:18 -0400

    feat: add date_from_dirname and is_conforming

--- tests/test_phish_rename.py
+++ tests/test_phish_rename.py
@@ -17,3 +17,37 @@ def load_module():
 
 
 pr = load_module()
+
+
+class TestDateFromDirname:
+    def test_yyyy_dash_mm_dd(self):
+        assert pr.date_from_dirname("2024-07-20 Xfinity Center Mansfield MA") == "2024-07-20"
+
+    def test_yyyy_underscore_mm_dd(self):
+        assert pr.date_from_dirname("2024_07_20 Xfinity Center Mansfield MA") == "2024-07-20"
+
+    def test_single_digit_month(self):
+        assert pr.date_from_dirname("1994_6_19 Somewhere") == "1994-06-19"
+
+    def test_single_digit_day(self):
+        assert pr.date_from_dirname("1994-12-6 Goleta, CA") == "1994-12-06"
+
+    def test_conforming_name_returns_none(self):
+        assert pr.date_from_dirname("Phish-2024-07-20.Xfinity.Center.Mansfield.MA.[2259]") is None
+
+    def test_undated_returns_none(self):
+        assert pr.date_from_dirname("Live Bait Vol 10") is None
+
+
+class TestIsConforming:
+    def test_canonical_name_is_conforming(self):
+        assert pr.is_conforming("Phish-2024-07-20.Xfinity.Center.Mansfield.MA.[2259]") is True
+
+    def test_old_dash_format_is_not_conforming(self):
+        assert pr.is_conforming("2024-07-20 Mansfield MA") is False
+
+    def test_missing_id_is_not_conforming(self):
+        assert pr.is_conforming("Phish-2024-07-20.Xfinity.Center.Mansfield.MA") is False
+
+    def test_underscore_format_is_not_conforming(self):
+        assert pr.is_conforming("2024_07_20 Xfinity Center Mansfield MA") is False
